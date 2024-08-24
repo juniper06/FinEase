4885170f2bbf1faaa0479bd71aebb7423a3a5957
EcFaburada <120457030+EcFaburada@users.noreply.github.com>
2024-08-24 15:03:34 +0800

    customer added

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { AuthModule } from './common/auth/auth.module';
 import { PrismaModule } from './common/prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { ProjectModule } from './project/project.module';
+import { CustomerModule } from './customer/customer.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule, UserModule, ProjectModule],
+  imports: [AuthModule, PrismaModule, UserModule, ProjectModule, CustomerModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/customer/dto/create-customer.dto.ts
@@ -0,0 +1,53 @@
+import { Transform } from 'class-transformer';
+import { IsNotEmpty, IsNumber, IsString, Length } from 'class-validator';
+
+export class CreateCustomerDto {
+  @IsNotEmpty()
+  @IsString()
+  type: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(1,20)
+  firstName: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(1,20)
+  lastName: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(1,30)
+  companyName: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(1,50)
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(1,11)
+  phoneNumber: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(1,30)
+  country: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(1,30)
+  city: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(1,30)
+  state: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @Transform(({ value }) => parseInt(value))
+  zipCode: number;
+}
